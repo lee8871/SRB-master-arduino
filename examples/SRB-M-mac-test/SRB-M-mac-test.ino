ea2213f113cb6c8a70ff3cc03eaa196e1c90bbf8
BIG-WHITE\lee8871 <lee8871@126.com>
2018-11-13 02:41:13 +0800

    编写了快速开始文档，这个文档还需要最后一部分，就是正常运行的Leonardo程序。

--- examples/SRB-M-mac-test/SRB-M-mac-test.ino
+++ examples/SRB-M-mac-test/SRB-M-mac-test.ino
@@ -2,125 +2,40 @@
 #define __ARDUINO__
 #include <lee.h>
 #include <Led.h>
+#include <PS2X_lib.h>  //for v1.6
+
 
 #include <SRB-M-tinynode.h>
 
 #define __MASTER__
-#define __STATIC_MAPPING_DECLEAR_ARDUINO__
+#define __MAPPING_DECLEAR__
 #include <readonly/SRB-app-cluster-test-share.h>
 #include <readonly/SRB-app-dmotor-share.h>
 
-// the setup routine runs once when you press reset:
-SrbTinyNode node6;
-test001::sDataRs node6_datas;
 
-SrbTinyNode node_motor;
-dMotor::sDataRs motor_data;
 
-void node6Init()
-{
-    node6.address = 0x09;
-    node6.datas = (uint8*)(&node6_datas);
-    node6.mapping[0] = (sStaticMapping*)(test001::mapping0);
-    node6.mapping[1] = (sStaticMapping*)(test001::mapping1);
-
-    node_motor.address = 0x03;
-    node_motor.datas = (uint8*)(&motor_data);
-    node_motor.mapping[0] = (sStaticMapping*)(dMotor::mapping0);
-    node_motor.mapping[1] = (sStaticMapping*)(dMotor::mapping1);
+//---------------- SRB 节点的初始化 ---------------------------------
+SrbTinyNode node_motor; //定义一个节点
+Du_Motor::sDataRs motor_data; //定义一个双电机模块的数据空间
+void nodeInit(){
+    //节点初始化设置
+    node_motor.address = 4;//设置节点地址
+    node_motor.datas = (uint8*)(&motor_data);//设置节点数据空间
+    node_motor.mapping[0] = (sStaticMapping*)(Du_Motor::mapping0);//设置节点访问映射表
+    node_motor.mapping[1] = (sStaticMapping*)(Du_Motor::mapping1);//设置节点访问映射表
+    //初始化完毕
 }
-int r = 11;   
-int g = 10;   
-int b = 9;   
 
-
-
-uint8 colorpPhase1024(uint16 phase);
-
-uint16 getSpeed1024(int value);
-
+//--------- system -----------------------------
 void setup() {
     SrbTinyNode::SRBinit();
-    node6Init();
-    pinMode(r, OUTPUT);
-    pinMode(g, OUTPUT);
-    pinMode(b, OUTPUT);
-    pinMode(A0, INPUT_PULLUP);
+    nodeInit();
 }
-
-// the loop routine runs over and over again forever:
-uint16 c_num = 50;
-uint16 temp;
-int x;
-int y;
-int z;
 
 void loop() {
-    c_num++;
-    c_num%=1024;
-    
-    x = analogRead(A1);
-    y = analogRead(A2);
-    z = analogRead(A3);
-    /***********************
-    node6_datas.color_in.r = x/4;
-    node6_datas.color_in.g = y/4;
-    node6_datas.color_in.b = z/4;  
-    //******************************/
-    if(digitalRead(A0) == LOW)
-    {
-    motor_data.ma.u16 = getSpeed1024(x);
-    motor_data.mb.u16 = getSpeed1024(y);
-    }
-    c_num = z;
-    node6_datas.color_in.r = colorpPhase1024((c_num)%1024);
-    node6_datas.color_in.g = colorpPhase1024((c_num + (1024/3))%1024);
-    node6_datas.color_in.b = colorpPhase1024((c_num + (1024/3)*2)%1024);  
-       
-    analogWrite(r, node6_datas.color_out.r);
-    analogWrite(g, node6_datas.color_out.g);
-    analogWrite(b, node6_datas.color_out.b);
-      
-    node6.access(0);
-    node_motor.access(1);
-    //************************************************************************/  
-    delay(2);
+    motor_data.ma.u16 = 140;
+    //前面的负号是因为两个车轮的机械结构对称,车前运行时,电机一个正转一个反转
+    motor_data.mb.u16 = 150;
+    node_motor.access(1);//将刚刚写好的数据发往电机.
+    delay(20);
 }
-uint16 getSpeed1024(int value)
-{
-    dMotor::sMotor m;
-    if(value>512+100)
-    {
-      m.speed = value-512;
-      m.dir = 0;
-    }
-    else if(value<512-100)
-    {      
-        m.speed = 512 - value;
-        m.dir = 3;
-    }
-    else
-    {
-      m.speed = 0;
-      m.dir = 1;
-    }
-    
-      m.speed *= 2;
-    return m.u16;
-}
-
-
-uint8 colorpPhase1024(uint16 phase)
-{
-    phase = phase%1024;
-    if(phase >= 256*3){
-      return(phase - 256*3);
-    }
-    else if(phase <= 255){
-      return 255 - phase;
-    }
-    else{
-      return 0;
-    }  
-}
-
